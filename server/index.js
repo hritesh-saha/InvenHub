28818784d5d12070ea7200fc622186a6fd4bffaa
Hritesh_Saha <hriteshsaha4@gmail.com>
2024-09-20 23:18:54 +0530

    adding put in cors

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(
   cors({
     origin: true,
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST","PUT"],
     allowedHeaders: "*",
     credentials: true,
   })
